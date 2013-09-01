e3d4b83d4b46a8363766d7b69c16d0457fab6a6b
Elden Armbrust <earmbrust@gmail.com>
2013-09-01 00:10:58 -0600

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+(function () {
+  //userecho code
+  var _ues = {
+  host:'mammock.userecho.com',
+  forum:'23089',
+  lang:'en',
+  tab_corner_radius:0,
+  tab_font_size:20,
+  tab_image_hash:'ZmVlZGJhY2s%3D',
+  tab_chat_hash:'Y2hhdA%3D%3D',
+  tab_alignment:'top',
+  tab_text_color:'#FFFFFF',
+  tab_text_shadow_color:'#00000055',
+  tab_bg_color:'#75A1BF',
+  tab_hover_color:'#8DC1E6'
+  };
+  
+
+  var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
+  _ue.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
+}())
