69c81cb702f473b99f6e3acd89cd753925359bc3
Elden Armbrust <earmbrust@gmail.com>
2013-09-02 02:18:50 -0600

    * Improved test speed * Added more tests for the library

--- test/test.js
+++ test/test.js
@@ -24,14 +24,14 @@ var winstonMock = {
 
 
 describe('Server', function(){
-    it ("server should instantiate", function(done){
+    it ("should instantiate", function(done){
         var Mammock = proxyquire('../src/lib/mammock.js', { 'winston': winstonMock });
         (typeof Mammock).should.equal("function");
         var server = new Mammock();
         done();
     });
-    it ("server should start and stop", function (done) {
-        this.timeout(700);
+    it ("should start and stop", function (done) {
+        this.timeout(100);
         var Mammock = proxyquire('../src/lib/mammock.js', { 'winston': winstonMock });
         (typeof Mammock).should.equal("function");
         var server = new Mammock();
@@ -41,6 +41,23 @@ describe('Server', function(){
 
             server.stop();
             done();
-        }, 500);
+        }, 50);
+    });
+    it ("should capture options", function (done) {
+        var Mammock = proxyquire('../src/lib/mammock.js', { 'winston': winstonMock });
+        var server = new Mammock({port: 5050, root: "test"});
+        server.options.port.should.equal(5050);
+        server.options.root.should.equal("test");
+        done();
+    });
+    it ("should call start() callback", function (done) {
+        var Mammock = proxyquire('../src/lib/mammock.js', { 'winston': winstonMock });
+        var server = new Mammock();
+        server.start(function () {
+          server.stop();
+          should.exist(server);
+          done();
+        });
+        //done();
     });
 });
